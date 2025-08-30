2760a34cb725519198b72ef81a33c3810b0af4b6
peaceminusone214 <peaceminusone214@gmail.com>
2025-08-30 20:14:30 +0700

    first commit

---
+++ BaiThucHanhBuoi1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BaiThucHanhBuoi1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BaiThucHanhBuoi1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9d678161-25fa-463a-8716-abad1f15db6e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TKForm/frmBai2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TKForm
+{
+    public partial class frmBai2 : Form
+    {
+        public frmBai2()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnChonHang_Click(object sender, EventArgs e)
+        {
+            var item = listBox1.SelectedItem;
+            listBox2.Items.Add(item);
+        }
+
+        private void btnBoHang_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Remove(listBox2.SelectedItem);
+        }
+
+        private void btnTinhTien_Click(object sender, EventArgs e)
+        {
+            int soTien = 0;
+
+            foreach (var hang in listBox2.Items)
+            { 
+                switch (hang)
+                {
+                    case "Chuột":
+                        soTien += 100000;
+                        break;
+                    case "Bàn phím":
+                        soTien += 150000;
+                        break;
+                    case "Máy in":
+                        soTien += 2000000;
+                        break;
+                    case "USB Kingmax":
+                        soTien += 200000;
+                        break;
+                    default:
+                        break;
+                }
+                lblSoTien.Text = soTien + " đồng";
+            }
+        }
+    }
+}
